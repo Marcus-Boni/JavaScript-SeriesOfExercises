6c3d0207bc232ce7aed64015f56bf84c4823e83a
Marcus Evandro Galvão Boni <115600640+Marcus-Boni@users.noreply.github.com>
2023-06-16 17:04:12 -0300

    Update index.js

--- Busca CEP API/index.js
+++ Busca CEP API/index.js
@@ -18,7 +18,7 @@ fetch("https://pokeapi.co/api/v2/pokemon")
   } catch (error) {
     console.log(error);
   }
-})();
+})(); 
 
 const getCepUrl = (cep) => `https://viacep.com.br/ws/${cep}/json/`;
 const cepInput = document.querySelector("#inputCEP");
@@ -31,13 +31,17 @@ const wrongCepMessage = wrongCep.querySelector(".content");
 const closeButton = wrongCep.querySelector(".close");
 
 cepInput.addEventListener("input", async (e) => {
-  if (cepInput.value.length === 8) {
-    const cep = Number(cepInput.value);
+  let cep = cepInput.value;
+  cep = cep.replace("-", "");
+  if (cep.length === 8) {
     try {
-      const dados = await fetch(getCepUrl(cep), { method: "GET" }).then(
-        (response) => response.json().catch((err) => console.log(err))
-      );
-      console.log(dados);
+      const dados = await fetch(getCepUrl(cep), {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .catch((err) => console.log(err));
+
       if (!dados.erro) {
         patioInput.value = dados.logradouro;
         neighborhoodInput.value = dados.bairro;
@@ -55,6 +59,11 @@ cepInput.addEventListener("input", async (e) => {
       wrongCep.style.transform = "translateY(0)";
       wrongCepMessage.innerText = err;
     }
+  } else {
+    patioInput.value = "";
+    neighborhoodInput.value = "";
+    cityInput.value = "";
+    stateInput.value = "";
   }
 });
 
@@ -67,3 +76,66 @@ closeButton.addEventListener("click", () => {
 //bairro
 //localidade
 //uf
+
+const cepInput = document.querySelector("#inputCEP");
+const patioInput = document.querySelector("#inputLogradouro");
+const neighborhoodInput = document.querySelector("#inputBairro");
+const cityInput = document.querySelector("#inputCidade");
+const stateInput = document.querySelector("#inputEstado");
+const wrongCep = document.querySelector("#bs-feedback");
+const wrongCepMessage = wrongCep.querySelector(".content");
+const closeButton = wrongCep.querySelector(".close");
+
+const getCepUrl = (cep) => `https://viacep.com.br/ws/${cep}/json/`;
+
+const getCepPromises = (cep) => {
+  const data = fetch(getCepUrl(cep), {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+  return data;
+};
+
+const renderInputs = ({ logradouro, bairro, localidade, uf }) => {
+  patioInput.value = logradouro;
+  neighborhoodInput.value = bairro;
+  cityInput.value = localidade;
+  stateInput.value = uf;
+};
+
+const displayError = () => {
+  patioInput.value = "";
+  neighborhoodInput.value = "";
+  cityInput.value = "";
+  stateInput.value = "";
+  wrongCepMessage.innerText = "Cep inexistente!";
+  wrongCep.style.transform = "translateY(0)";
+};
+
+const verifyInput = async (e) => {
+  let cep = e.currentTarget.value;
+  cep = cep.replace("-", "");
+
+  if (isNaN(cep) || cep.length !== 8) return;
+  
+
+  try {
+    const dados = await getCepPromises(cep);
+    if (dados.erro) {
+      displayError();
+    } else {
+      renderInputs(dados);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const hideError = () => {
+  wrongCep.removeAttribute("style");
+};
+
+cepInput.addEventListener("input", verifyInput);
+closeButton.addEventListener("click", hideError);
